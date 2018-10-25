afbad670ea585964f5c8653d352389663a1c2a2b
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-10-25 11:07:09 -0400

    fixed my app.js bc it wasnt working

--- db/connection.js
+++ db/connection.js
@@ -3,5 +3,6 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI).then(() => {
  console.log('Connected to MongoDB')
- 
+
  module.exports = mongoose
+ 
