50d12101d4e16093d5162bb1a76ea617c2c683f3
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-10-25 23:31:09 -0400

    finished my models?

--- models/Bad.js
+++ models/Bad.js
@@ -2,7 +2,9 @@ const mongoose = require('../db/connection')
 const Schema = mongoose.Schema
 
 const Bad = new Schema({
-
+    name: String,
+    image: String,
+    description: String
 })
 
 module.exports = mongoose.model('Bad', Bad)
--- models/Good.js
+++ models/Good.js
@@ -2,7 +2,9 @@ const mongoose = require('../db/connection')
 const Schema = mongoose.Schema
 
 const Good = new Schema ({
-
+    name: String,
+    image: String,
+    description: String
 })
 
 module.exports = mongoose.model('Good', Good)
