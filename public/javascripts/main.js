421b4d86feaac45b84ea722a017a273d18d4e3f1
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-10-28 21:44:57 -0400

    teting heroku

---
+++ public/javascripts/main.js
@@ -0,0 +1,16 @@
+let newGood = document.querySelector('#newGood')
+let newBad = document.querySelector('#newBad')
+let scoreboard = document.querySelector('#scoreBoard')
+let points = 0
+
+newGood.addEventListener('click', function () {
+    points += 10
+    scoreboard.innerHTML = points
+
+})
+
+newBad.addEventListener('click', function () {
+    points -= 100
+    scoreboard.innerHTML = points
+
+})
